1d5ea1b02bc1cbbc2ee1e9564b5219ba7af6d69c
taufiqdp <topexdw@gmail.com>
2025-02-01 11:03:07 +0700

    done

--- Labfiles/01-analyze-text/Python/text-analysis/text-analysis.py
+++ Labfiles/01-analyze-text/Python/text-analysis/text-analysis.py
@@ -2,7 +2,8 @@ from dotenv import load_dotenv
 import os
 
 # Import namespaces
-
+from azure.core.credentials import AzureKeyCredential
+from azure.ai.textanalytics import TextAnalyticsClient
 
 def main():
     try:
@@ -12,7 +13,8 @@ def main():
         ai_key = os.getenv('AI_SERVICE_KEY')
 
         # Create client using endpoint and key
-
+        credential = AzureKeyCredential(ai_key)
+        ai_client = TextAnalyticsClient(endpoint=ai_endpoint, credential=credential)
 
         # Analyze each text file in the reviews folder
         reviews_folder = 'reviews'
@@ -23,18 +25,37 @@ def main():
             print('\n' + text)
 
             # Get language
+            detected_language = ai_client.detect_language(documents=[text])[0]
+            print('Language:', detected_language.primary_language.name)
 
 
             # Get sentiment
+            sentimentAnalysis = ai_client.analyze_sentiment(documents=[text])[0]
+            print('Sentiment:', sentimentAnalysis.sentiment)
 
 
             # Get key phrases
+            phrases = ai_client.extract_key_phrases(documents=[text])[0].key_phrases
+            if len(phrases) > 0:
+                print('Key Phrases:')
+                for phrase in phrases:
+                    print('-', phrase)
 
 
             # Get entities
+            entities = ai_client.recognize_entities(documents=[text])[0].entities
+            if len(entities) > 0:
+                print('Entities:')
+                for entity in entities:
+                    print('-', entity.text, '(', entity.category, ')')
 
 
             # Get linked entities
+            linked_entities = ai_client.recognize_linked_entities(documents=[text])[0].entities
+            if len(linked_entities) > 0:
+                print('Linked Entities:')
+                for entity in linked_entities:
+                    print('-', entity.name, '(', entity.url, ')')
 
 
 
